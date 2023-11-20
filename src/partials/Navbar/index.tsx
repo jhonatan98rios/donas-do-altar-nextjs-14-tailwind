9f36af67c1c773ce93ec77065b97b1449c201b0a
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2023-11-20 12:10:27 -0300

    responsiveness fixes

--- src/partials/Navbar/index.tsx
+++ src/partials/Navbar/index.tsx
@@ -4,7 +4,7 @@ import { DropdownMenu } from "@/components/DropdownMenu"
 
 export const Navbar = () => {
   return (
-    <navbar className={`flex flex-col items-center fixed top-0 w-[calc(100%-32px)] z-10 pt-6 sm:h-52 sm:w-full bg-slate-300 sm:relative`}>
+    <navbar className={`flex flex-col items-center fixed top-0 w-[calc(100%-32px)] z-10 pt-6 sm:h-52 sm:w-full bg-white sm:relative`}>
       <div className="flex items-center justify-center mb-8">
         <div className="w-24 sm:w-48 bg-slate-500 h-10 sm:h-20">
 
