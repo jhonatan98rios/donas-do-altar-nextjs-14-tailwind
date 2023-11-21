cdaebb6bb850b4213cf09d2dab0b2bf8579dbde8
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2023-11-21 20:54:30 -0300

    removing unused files

--- src/components/ServiceCard/index.tsx
+++ src/components/ServiceCard/index.tsx
@@ -1,21 +1,16 @@
-import { BaseImage } from "@/utils/baseImage"
-import { ResponsiveImage } from "../ResponsiveImage"
 
 type ServiceCardProps = {
-  baseImage: BaseImage
+  image: string
+  alt: string
   title: string
   description: string
 }
 
-export function ServiceCard({ baseImage, title, description }: ServiceCardProps) {
+export function ServiceCard({ image, alt, title, description }: ServiceCardProps) {
 
   return (
     <article className="w-72 sm:w-96 h-[32rem] sm:h-[38rem] bg-white flex flex-col p-4 mx-2 sm:mx-4 border border-solid border-gray-400">
-
-      <ResponsiveImage
-        sources={baseImage.src}
-        alt={baseImage.alt}
-      />
+      <img src={ image } alt={alt} title={title} />
 
       <h3 className="my-5 text-xl sm:text-2xl text-gray-500 font-bold">
         {title}
--- src/partials/Services/index.tsx
+++ src/partials/Services/index.tsx
@@ -1,6 +1,5 @@
 import { Carousel } from "@/components/Carousel";
 import { ServiceCard } from "@/components/ServiceCard";
-import { baseImage } from "@/utils/baseImage";
 
 export function Services() {
 
@@ -14,91 +13,43 @@ export function Services() {
         <Carousel>
 
           <ServiceCard 
-            baseImage={
-              baseImage(
-                [
-                  { src: "/services/assessoria_completa.png, /services/assessoria_completa@2x.png 2x", query: '(min-width: 769px)' },
-                  { src: "/services/mobile/assessoria_completa.png, /services/mobile/assessoria_completa@2x.png 2x" },
-                ],
-                "", 
-                ""
-              )
-            }
+            image={"/services/assessoria_completa.png"}
+            alt=""
             title="Assessoria Completa"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           />
 
           <ServiceCard 
-            baseImage={
-              baseImage(
-                [
-                  { src: "/services/assessoria_personalizada.png, /services/assessoria_personalizada@2x.png 2x", query: '(min-width: 769px)' },
-                  { src: "/services/mobile/assessoria_personalizada.png, /services/mobile/assessoria_personalizada@2x.png 2x" },
-                ], 
-                "", 
-                ""
-              )
-            }
+            image={"/services/assessoria_personalizada.png"}
+            alt=""
             title="Assessoria Personalizada"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           />
 
-          <ServiceCard 
-            baseImage={
-              baseImage(
-                [
-                  { src: "/services/assessoria_final.png, /services/assessoria_final@2x.png 2x", query: '(min-width: 769px)' },
-                  { src: "/services/mobile/assessoria_final.png, /services/mobile/assessoria_final@2x.png 2x" },
-                ], 
-                "", 
-                ""
-              )
-            }
+          <ServiceCard
+            image={"/services/assessoria_final.png"}
+            alt=""
             title="Assessoria Final"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           />
 
           <ServiceCard 
-            baseImage={
-              baseImage(
-                [
-                  { src: "/services/curso_de_assessoria.png, /services/curso_de_assessoria@2x.png 2x", query: '(min-width: 769px)' },
-                  { src: "/services/mobile/curso_de_assessoria.png, /services/mobile/curso_de_assessoria@2x.png 2x" },
-                ], 
-                "", 
-                ""
-              )
-            }
+            image={"/services/curso_de_assessoria.png"}
+            alt=""
             title="Curso de Assessoria"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           />
 
           <ServiceCard 
-            baseImage={
-              baseImage(
-                [
-                  { src: "/services/debutantes.png, /services/debutantes@2x.png 2x", query: '(min-width: 769px)' },
-                  { src: "/services/mobile/debutantes.png, /services/mobile/debutantes@2x.png 2x" },
-                ], 
-                "", 
-                ""
-              )
-            }
+            image={"/services/debutantes.png"}
+            alt=""
             title="Debutantes"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           />
 
           <ServiceCard 
-            baseImage={
-              baseImage(
-                [
-                  { src: "/services/eventos_sociais_e_coorporativos.png, /services/eventos_sociais_e_coorporativos@2x.png 2x", query: '(min-width: 769px)' },
-                  { src: "/services/mobile/eventos_sociais_e_coorporativos.png, /services/mobile/eventos_sociais_e_coorporativos@2x.png 2x" },
-                ], 
-                "", 
-                ""
-              )
-            }
+            image={"/services/eventos_sociais_e_coorporativos.png"}
+            alt=""
             title="Eventos Sociais e Coorporativos"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           />
