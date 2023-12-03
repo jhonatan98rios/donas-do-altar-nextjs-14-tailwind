8bbc9441f2e4e22e7a6cd3f0c64e19daab7b079c
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2023-12-03 00:08:31 -0300

    fix

--- src/partials/About/index.tsx
+++ src/partials/About/index.tsx
@@ -2,7 +2,7 @@ export function About() {
   return (
     <section className="flex flex-col text-center justify-center items-center bg-white" id="sobre">
       <div className="xl:w-3/4 md:w-3/4 w-11/12 py-10 lg:py-16">
-        <h2 className="mb-8 sm:mb-12 text-xl sm:text-2xl text-gray-500"> SOBRE NÓS </h2>
+        <h2 className="mb-8 text-xl sm:text-2xl text-gray-500"> SOBRE NÓS </h2>
         <p className="text-base sm:text-xl text-gray-700">
           A Donas do Altar tem como propósito realizar seus sonhos. É para isso que trabalhamos e é isso que nos move e isso fica nítido na forma como planejamos e entregamos nossos eventos reais cuidando de cada detalhe com muito carinho, sofisticação e muito profissionalismo. <br /> Nossa meta é que todos os clientes que passem por aqui tenham uma experiência inesquecível e, que possam relembrar cada momento com muito carinho. Para isso, desenvolvemos alguns métodos que permitem que o planejamento seja fácil, rápido, seguro e, por que não, divertido? O resultado final: tudo do jeitinho que vocês desejaram! 
         </p>
